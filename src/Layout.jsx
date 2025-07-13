b48eccaed2964d791e673e270db799dec0a70081
unknown <aviya1221@gmail.com>
2025-07-13 22:35:14 +0300

    Updated role management, restructured Layout and MainScreen, added basic Router and Link integration, and modified Sidebar and Members components

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Nav from './Sidebar'
-import MainScreen from './MainScreen'
-import Sidebar from './Sidebar';
+import React from "react";
+import Nav from "./Sidebar";
+import MainScreen from "./MainScreen";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import UserScreen from "./userComp/UserScreen.jsx";
+import AdminScreen from "./adminComp/AdminScreen.jsx";
+import Members from "./adminComp/Members.jsx";
+import Sidebar from "./Sidebar.jsx";
 
 export default function Layout() {
   return (
- <>
-         <div className="position-fixed top-0 start-0 vh-100 rounded    " style={{ width: '220px' }}>
- <Sidebar></Sidebar>
-   </div>
-<div className="flex-grow-1 p-4">
- <MainScreen></MainScreen>
-   </div>
-
+    <>
+      <div className="flex-grow-1 p-4" style={{ marginLeft: "13.75rem" }}>
+        <BrowserRouter>
+          <div
+            className=" d-flex position-fixed top-0 start-0 vh-100 rounded    "
+            style={{ width: "13.75rem" }}
+          >
+            <Sidebar />
+          </div>
+          <div>
+            <Routes>
+              <Route path="/admin" element={<AdminScreen />} />
+              <Route path="/admin/members" element={<Members />} />
+              <Route path="/user" element={<UserScreen />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
     </>
-  )
+  );
 }
