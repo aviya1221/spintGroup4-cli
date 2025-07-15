a2b988c51d41bb5a400d312304d507448630e380
unknown <aviya1221@gmail.com>
2025-07-15 14:26:37 +0300

    fix user screen bug

--- src/userComp/UserDetail.jsx
+++ src/userComp/UserDetail.jsx
@@ -5,29 +5,32 @@ import AgreeButton from "./userButtons/AgreeButton";
 
 export default function UserDetails({ refs }) {
   return (
-    <Container
-  fluid
-  className="p-4"
-  style={{
-    backgroundColor: "#495057",
-    color: "#fff"
-  }}
->
+     <Container
+      fluid
+      className="p-4 text-white"
+    >
       <h3>Additional user details</h3>
+
       <label>Phone</label>
       <input ref={refs.phoneRef} className="form-control mb-2" defaultValue="+972-50-1234567" />
+
       <label>LinkedIn</label>
       <input ref={refs.linkedinRef} className="form-control mb-2" defaultValue="https://example.com" />
+
       <label>Email</label>
       <input ref={refs.emailRef} type="email" className="form-control mb-2" defaultValue="4Community@gmail.com" />
+
       <label>Years of experience</label>
       <input ref={refs.yearsRef} type="number" className="form-control mb-2" defaultValue="10" />
+
       <label>Skills</label>
       <input ref={refs.skillsRef} className="form-control mb-2" defaultValue="HTML,CSS" />
+
       <label>Last company</label>
       <input ref={refs.LastCompRef} className="form-control mb-2" defaultValue="Target,Meta" />
+
       <label className="mt-3">notification</label>
-      <div style={{ display: "flex", justifyContent: "center", marginBottom: "3rem" }}>
+      <div className="d-flex justify-content-center mb-4">
         <AgreeButton />
       </div>
 
