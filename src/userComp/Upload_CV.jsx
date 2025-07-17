dcdad50529813f5ceeb33e463d5e7d0315903a3f
nerya.reznikovich <neryarez@gmail.com>
2025-07-17 10:38:18 +0300

    api key

--- src/userComp/Upload_CV.jsx
+++ src/userComp/Upload_CV.jsx
@@ -14,7 +14,7 @@ export function CvUploadProvider({ children }) {
   const navigate = useNavigate();
 
   const openai = new OpenAI({
-    apiKey: import.meta.env.VITE_API_KEY,
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true,
   });
 
